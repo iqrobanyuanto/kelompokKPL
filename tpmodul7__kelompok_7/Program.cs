461a2f45fcbfec7ac30a8017ed15803ddff5c2af
iqrobanyuanto <iqrobanyuanto12345@gmail.com>
2023-04-02 05:06:06 +0700

    menambahkan class DataMahasiswa1302213061

--- tpmodul7__kelompok_7/Program.cs
+++ tpmodul7__kelompok_7/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text.Json;
+
+public class Program
+{
+     public static void Main(string[] args)
+     {
+         DataMahasiswa1302213061 dat = new DataMahasiswa1302213061();
+        Mahasiswa run = dat.ReadJSON<Mahasiswa>("tp7_1_1302213061.json");
+        Console.WriteLine(run.nama.depan + " "+ run.nama.belakang);
+        Console.WriteLine(run.nim);
+        Console.WriteLine(run.fakultas);
+    }
+}
