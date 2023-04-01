b4bec53c48b36e6afdd3d560ac29c430156ae41e
Alif T <alift929@gmail.com>
2023-04-02 00:41:33 +0700

    menambahkan class DataMahasiswa1302213011, Mahasiswa, dan Nama

--- tpmodul7__kelompok_7/Program.cs
+++ tpmodul7__kelompok_7/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        DataMahasiswa1302213011 dataMhs = new DataMahasiswa1302213011();
+        dataMhs.ReadJSON();
+    }
+}
