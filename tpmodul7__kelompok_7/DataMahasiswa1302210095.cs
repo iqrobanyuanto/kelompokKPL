cdb1b2ef2c663d9aad0f8203f4ed754120b0e32c
raimess11 <rahma.sakti24@gmail.com>
2023-04-03 04:55:16 +0700

    menambahkan class DataMahasiswa1302210095

---
+++ tpmodul7__kelompok_7/DataMahasiswa1302210095.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul7__kelompok_7
+{
+    internal class DataMahasiswa1302210095
+    {
+        public void ReadJSON<tipe>(string filename)
+        {
+            //generate file path
+            string path = "\\" + filename;
+
+            //membaca file json
+            string read = File.ReadAllText(path);
+
+            //mengubah file json yang sudah di baca menjadi object
+            tipe obj = JsonSerializer.Deserialize<tipe>(read);
+
+            if(obj is int)
+            {
+                Console.WriteLine(
+                    "Nama " + obj.nama.depan + " " + obj.nama.belakang +
+                    " dengan nim " + obj.nim +
+                    " dari fakultas " + obj.fakultas 
+                    );
+            }
+
+        }
+    }
+}
