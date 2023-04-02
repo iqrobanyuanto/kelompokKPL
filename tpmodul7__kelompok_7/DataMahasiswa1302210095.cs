093a42fbc055efd73ecc21bf62055e72dcf5a759
raimess11 <rahma.sakti24@gmail.com>
2023-04-03 05:24:03 +0700

    menambahkan object struct mahasiswa dan nama

--- tpmodul7__kelompok_7/DataMahasiswa1302210095.cs
+++ tpmodul7__kelompok_7/DataMahasiswa1302210095.cs
@@ -9,7 +9,7 @@ namespace tpmodul7__kelompok_7
 {
     internal class DataMahasiswa1302210095
     {
-        public void ReadJSON<tipe>(string filename)
+        public void ReadJSON(string filename)
         {
             //generate file path
             string path = "\\" + filename;
@@ -18,17 +18,54 @@ namespace tpmodul7__kelompok_7
             string read = File.ReadAllText(path);
 
             //mengubah file json yang sudah di baca menjadi object
-            tipe obj = JsonSerializer.Deserialize<tipe>(read);
+            Mahasiswa1302210095 obj = JsonSerializer.Deserialize<Mahasiswa1302210095>(read);
 
-            if(obj is int)
-            {
-                Console.WriteLine(
-                    "Nama " + obj.nama.depan + " " + obj.nama.belakang +
-                    " dengan nim " + obj.nim +
-                    " dari fakultas " + obj.fakultas 
-                    );
-            }
+            Console.WriteLine(
+                "Nama " + obj.nama.depan + " " + obj.nama.belakang +
+                " dengan nim " + obj.nim +
+                " dari fakultas " + obj.fakultas 
+                );
 
         }
+    }
+}
+
+
+struct Mahasiswa1302210095
+{
+    public Nama1302210095 nama { get; set; }
+    public int nim { get; set; }
+    public string fakultas { get; set; }
+    
+    public Mahasiswa1302210095()
+    {
+        this.nama = new Nama1302210095();
+        this.nim = 0;
+        this.fakultas = "";
+    }
+
+    public Mahasiswa1302210095(Nama1302210095 nama, int nim, string fakultas)
+    {
+        this.nama = nama;
+        this.nim = nim;
+        this.fakultas = fakultas;
+    }
+}
+
+struct Nama1302210095
+{
+    public string depan { get; set; }
+    public string belakang { get; set; }
+
+    public Nama1302210095()
+    {
+        this.depan = "";
+        this.belakang = "";
+    }
+
+    public Nama1302210095(string depan, string belakang)
+    {
+        this.depan = depan;
+        this.belakang = belakang;
     }
 }
