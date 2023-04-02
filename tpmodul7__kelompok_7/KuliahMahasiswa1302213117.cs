3dd15f20d178d66b6aa73452474345127ab71014
Regyyyy <regyrahman@gmail.com>
2023-04-03 03:40:22 +0700

    menambahkan class KuliahMahasiswa1302213117
    menambahkan class KuliahMahasiswa1302213117

---
+++ tpmodul7__kelompok_7/KuliahMahasiswa1302213117.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul7__kelompok_7
+{
+    public class KuliahMahasiswa1302213117
+    {
+        public List<Course> courses { get; set; }
+        public void ReadJSON()
+        {
+            string folder = "D:\\Telkom University\\Semester 4\\Konstruksi Perangkat Lunak\\Tubes Kelompok\\kelompokKPL\\tpmodul7__kelompok_7";
+            string hasilBaca = File.ReadAllText(folder + "\\tp7_2_1302213117.json");
+            KuliahMahasiswa1302213117 data = JsonSerializer.Deserialize<KuliahMahasiswa1302213117>(hasilBaca);
+            Console.WriteLine("Daftar mata kuliah yang diambil:");
+            for (int i = 0; i < data.courses.Count; i++)
+            {
+                Console.WriteLine("MK "+(i+1)+" " + data.courses[i].code + " - " + data.courses[i].name);
+            }
+        }
+    }
+
+    public class Course
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+        public Course() { }
+        public Course(string code, string name)
+        {
+            this.code = code;
+            this.name = name;
+        }
+    }
+}
--- tpmodul7__kelompok_7/Program.cs
+++ tpmodul7__kelompok_7/Program.cs
@@ -2,45 +2,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 using System.Text.Json;
+using tpmodul7__kelompok_7;
 
 public class Program
 {
      public static void Main(string[] args)
      {
 
-        Console.WriteLine("Tugas JSON Deserialization 2 - Deva Angela Felisitas"+Environment.NewLine);
-        KuliahMahasiswa1302213111 daftarMatkul = new KuliahMahasiswa1302213111();
-        daftarMatkul.ReadJSON();
-
-
-        Console.WriteLine("Tugas 1 - Iqro Banyuanto");
-        DataMahasiswa1302213061 dat = new DataMahasiswa1302213061();
-        Mahasiswa run = dat.ReadJSON<Mahasiswa>("tp7_1_1302213061.json");
-        Console.WriteLine("Nama "+run.nama.depan + " "+ run.nama.belakang+" dengan nim "+ run.nim+" dari fakultas "+ run.fakultas);
-        Console.WriteLine("\nTugas 2 - Iqro Banyuanyo");
-        KuliahMahasiswa13022130061 dat2 = new KuliahMahasiswa13022130061();
-        matkul run2 = dat2.ReadJSON("tp7_2_1302213061.json");
-        for(int i = 0; i < run2.courses.Count; i++)
-        {
-            Console.WriteLine("MK "+(i+1)+" "+run2.courses[i].code+" - "+ run2.courses[i].name);
-        }
-
-
-        Console.WriteLine("Tugas JSON Deserialization 1 - Alif Taufiqurrahman");
-        DataMahasiswa1302213011 dataMhs = new DataMahasiswa1302213011();
-        dataMhs.ReadJSON();
-
-        Console.WriteLine("Tugas JSON Deserialization 2 - Alif Taufiqurrahman");
-        KuliahMahasiswa1302213011 kuliah = new KuliahMahasiswa1302213011();
-        kuliah.ReadJSON();
-        
-
-        Console.WriteLine("Tugas JSON Deserialization 1 - Deva Angela Felisitas");
-        DataMahasiswa1302213111 dataMahasiswa = new DataMahasiswa1302213111();
-        dataMahasiswa.ReadJSON();
-
-
-
-     }
+        KuliahMahasiswa1302213117 regy2 = new KuliahMahasiswa1302213117();
+        regy2.ReadJSON();
+    }
 }
 
